5d533b5ddecff017702bfa0f4bb2fc27006c310b
Rosie0601 <129477710+Rosie0601@users.noreply.github.com>
2023-06-11 21:21:39 +0800

    Update script.js

--- parallaxScrollingWebsite01/script.js
+++ parallaxScrollingWebsite01/script.js
@@ -1,8 +1,8 @@
 let text = document.getElementById('text');
 let leaf = document.getElementById('leaf');
-let hill1 = document.getElementById('hill1');
-let hill4 = document.getElementById('hill4');
-let hill5 = document.getElementById('hill5');
+let 左 = document.getElementById('左');
+let 右 = document.getElementById('右');
+let 中 = document.getElementById('中');
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
@@ -10,7 +10,7 @@ window.addEventListener('scroll', () => {
     text.style.marginTop = value * 1.5 + 'px';
     leaf.style.top = value * -1.5 + 'px';
     leaf.style.left = value * 1.5 + 'px';
-    hill5.style.left = value * 1.5 + 'px';
-    hill4.style.left = value * -1.5 + 'px';
-    hill1.style.top = value * 0.5 + 'px';
+    左.style.left = value * 1.5 + 'px';
+    右.style.left = value * -1.5 + 'px';
+    中.style.top = value * 0.5 + 'px';
 });
